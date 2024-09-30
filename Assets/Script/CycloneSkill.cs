bcf8206c19a4d6dfe833ddeb4197097ec52f4805
Hoang Pham <pcm230304@gmail.com>
2024-09-30 16:47:55 +0700

    done cyclone animation, spawner

--- Assets/Script/CycloneSkill.cs
+++ Assets/Script/CycloneSkill.cs
@@ -5,22 +5,38 @@ using UnityEngine;
 
 public class CycloneSkill : MonoBehaviour
 {
-	[SerializeField] private float pullForce;
-	[SerializeField] private float pullDuration;
-	//[SerializeField] private float skillCD;
-	[SerializeField] private int damage;
+	[SerializeField] private float pullForce = 10f;
+	[SerializeField] private float pullDuration = 5f;
+	[SerializeField] private int damage = 1;
+	[SerializeField] private Transform pullCenter;
+	[SerializeField] private float randomRadius = 3f;
 
-	public float skillCD;
+	public float skillCD = 2f;
 	// Cooldown time after pulling
 	private bool isPulling = false;
 	private bool isOnCooldown = false;
 	public bool IsActingComplete { get; private set; }
 
-	[SerializeField] private Transform pullCenter;
-
 	public void Run()
 	{
 		StartCoroutine(PullCycle());
+		SetRandomPullCenter();
+	}
+
+	private void SetRandomPullCenter()
+	{
+		if (PlayerController3.Instance != null)
+		{
+			// Get the player's position
+			Vector3 playerPosition = PlayerController3.Instance.transform.position;
+
+			// Generate a random position near the player within the specified radius
+			Vector2 randomDirection = UnityEngine.Random.insideUnitCircle.normalized;
+			Vector3 randomPosition = playerPosition + new Vector3(randomDirection.x, randomDirection.y, 0) * UnityEngine.Random.Range(0, randomRadius);
+
+			// Set the pullCenter position to the new random position
+			pullCenter.position = randomPosition;
+		}
 	}
 
 	private IEnumerator PullCycle()
