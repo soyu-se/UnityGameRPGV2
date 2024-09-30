c581d36abd318e1fedce5e5758cd3bc8a0cd4351
DucHuy <huypd200604@gmail.com>
2024-09-30 16:10:41 +0700

    Weapon interface+UI

--- Assets/Script/ActiveInventory.cs
+++ Assets/Script/ActiveInventory.cs
@@ -16,6 +16,7 @@ public class ActiveInventory : MonoBehaviour
     private void Start()
     {
         playerControls.Inventory.Keyboard.performed += ctx => ToggleActiveSlot((int)ctx.ReadValue<float>());
+        ToggleActiveHighlight(0);
     }
 
     private void OnEnable()
@@ -38,5 +39,24 @@ public class ActiveInventory : MonoBehaviour
         }
 
         this.transform.GetChild(indexNum).GetChild(0).gameObject.SetActive(true);
+        ChangeActiveWeapon();
+    }
+    private void ChangeActiveWeapon()
+    {
+        if (ActiveWeapon.Instance.CurrentActiveWeapon != null) { Destroy(ActiveWeapon.Instance.CurrentActiveWeapon.gameObject); }
+        if (!transform.GetChild(activeSlotIndexNum).GetComponentInChildren<InventorySlot>())
+        {
+            ActiveWeapon.Instance.WeaponNull();
+            return;
+        }
+        
+        GameObject weaponToSpam = transform.GetChild(activeSlotIndexNum).
+            GetComponentInChildren<InventorySlot>().GetWeaponInfo().weaponPrefab;
+
+        GameObject newWeapon = Instantiate(weaponToSpam, ActiveWeapon.Instance.transform.position, Quaternion.identity);
+
+        newWeapon.transform.parent = ActiveWeapon.Instance.transform;
+
+        ActiveWeapon.Instance.NewWeapon(newWeapon.GetComponent<MonoBehaviour>());
     }
 }
