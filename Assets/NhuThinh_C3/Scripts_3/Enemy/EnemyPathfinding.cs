dbf58348a8996e1117dea9412568ccd64522f048
Nhu Thinh <nhuthinh10a3@gmail.com>
2024-10-03 12:49:32 +0700

    fix

--- Assets/NhuThinh_C3/Scripts_3/Enemy/EnemyPathfinding.cs
+++ Assets/NhuThinh_C3/Scripts_3/Enemy/EnemyPathfinding.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class EnemyPathfinding : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float collisionDelay = 0.1f;
 
     private Rigidbody2D rb;
     private Vector2 moveDir;
     private Knockback knockback;
     private SpriteRenderer spriteRenderer;
-
+    private bool isColliding = false; 
 
     private void Awake()
     {
@@ -21,7 +22,7 @@ public class EnemyPathfinding : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (knockback.GettingKnockedBack) { return; }
+        if (knockback.GettingKnockedBack || isColliding) { return; }
 
         rb.MovePosition(rb.position + moveDir * (moveSpeed * Time.fixedDeltaTime));
 
@@ -29,7 +30,7 @@ public class EnemyPathfinding : MonoBehaviour
         {
             spriteRenderer.flipX = true;
         }
-        else //if (moveDir.x > 0)
+        else if (moveDir.x > 0)
         {
             spriteRenderer.flipX = false;
         }
@@ -37,12 +38,27 @@ public class EnemyPathfinding : MonoBehaviour
 
     public void MoveTo(Vector2 targetPosition)
     {
-        //moveDir = targetPosition;
         moveDir = (targetPosition - rb.position).normalized;
     }
 
     public void StopMoving()
     {
-        moveDir = Vector3.zero;
+        moveDir = Vector2.zero;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isColliding)
+        {
+            StartCoroutine(ChangeDirectionAfterCollision());
+        }
+    }
+
+    private IEnumerator ChangeDirectionAfterCollision()
+    {
+        isColliding = true;
+        yield return new WaitForSeconds(collisionDelay);
+        moveDir = new Vector2(-moveDir.x, -moveDir.y);
+        isColliding = false;
     }
 }
